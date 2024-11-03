62c77dc3bb2c1c637dd29533522168ac68562e72
Manuela Di Dario <didariomanuela@gmail.com>
2024-11-03 21:24:56 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CalenderApp from './components/CalenderApp'
+import './components/CalenderApp.css'
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className='container'>
+      <CalenderApp/>
+    </div>
+  )
+}
+
+export default App
