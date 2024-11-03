7e12416540651b689e715b84fbcb45d06d6c0c35
Manuela Di Dario <didariomanuela@gmail.com>
2024-11-03 23:13:11 +0100

    final review of packages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://manudd25.github.io/React-Calendar/' 
+  base: '/React-Calendar/' 
 });
 
 
