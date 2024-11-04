3da387a02c1f7c90d70520c9865993bd72017a5d
Manuela Di Dario <didariomanuela@gmail.com>
2024-11-04 13:01:02 +0100

    made some changes in the packages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/React-Calendar/',
   plugins: [react()],
-  base: '/React-Calendar/' 
 });
-
-
